2577859b924860fb25680eddd679d5d922dbbc0d
Fatma <fmutlu3002@gmail.com>
2024-02-04 21:14:59 +0300

    design fixes

--- src/components/(admin)/search/Search.tsx
+++ src/components/(admin)/search/Search.tsx
@@ -5,32 +5,32 @@ import { MdSearch } from 'react-icons/md'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 
-interface SearchProps{
-    placeholder:string;
-    paramKey:string;
-  }
+interface SearchProps {
+    placeholder: string;
+    paramKey: string;
+}
 
-const Search = ( {placeholder,paramKey}:SearchProps) => {
+const Search = ({ placeholder, paramKey }: SearchProps) => {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
-     
-    const {replace} =useRouter();
 
-    const handleSearch=(e: ChangeEvent<HTMLInputElement>)=>{
+    const { replace } = useRouter();
+
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
         const params = new URLSearchParams(searchParams);
 
-        if(e.target.value){
+        if (e.target.value) {
 
-            params.set(paramKey, e.target.value);
-        }else{
+            e.target.value.length % 2 !=0 && params.set(paramKey, e.target.value);
+        } else {
             params.delete(paramKey);
         }
-    
+
         replace(`${pathname}?${params}`);
     }
 
-   
+
     return (
         <div className={styles.container}>
             <div className={styles.search}>
